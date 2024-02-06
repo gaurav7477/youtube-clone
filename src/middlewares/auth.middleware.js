82ecbf32c7847bf5d17438708352a968515a7df5
gaurav7477 <bairagi7477@gmail.com>
2024-02-06 16:00:17 +0530

    login,logout and auth middleware implemented

---
+++ src/middlewares/auth.middleware.js
@@ -0,0 +1,32 @@
+import { asyncHandler } from "../utils/asyncHandler.js";
+import jwt from "jsonwebtoken";
+import { User } from "../models/user.model.js";
+import { ApiError } from "../utils/ApiError.js";
+
+export const verifyJwtToken = asyncHandler(async (req, _ ,next) => {
+  // for web dev req.cookies but for mobile app dev use header
+  try {
+    const token =
+      req.cookies?.accessToken ||
+      req.header("Authorization")?.replace("Bearer", "");
+  
+    if (!token) {
+      throw new ApiError(401, "unauthorized request");
+    }
+  
+    const decodedToken = jwt.verify(token, process.env.ACCESS_TOKEN_SECRET);
+  
+    const user = await User.findById(decodedToken?._id).select(
+      "-password -refreshToken"
+    );
+  
+    if (!user) {
+      throw new ApiError(401, "Invalid Access Token");
+    }
+  
+    req.user = user
+    next()
+  } catch (error) {
+    throw new ApiError(401, error?.message || "Invalid access token") 
+  }
+});
